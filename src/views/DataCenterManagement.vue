c977e08e57c78c72d52cee42683c8f0f588f3d4b
keepersay <8269838+keepersay@user.noreply.gitee.com>
2025-06-23 11:33:28 +0800

    修改机房管理模块，将联系人、电话和邮箱设置为非必填字段

--- src/views/DataCenterManagement.vue
+++ src/views/DataCenterManagement.vue
@@ -229,14 +229,14 @@ const rules = {
     { required: true, message: '请选择状态', trigger: 'change' }
   ],
   contactPerson: [
-    { required: true, message: '请输入联系人', trigger: 'blur' }
+    { required: false, message: '请输入联系人', trigger: 'blur' }
   ],
   contactPhone: [
-    { required: true, message: '请输入联系电话', trigger: 'blur' },
+    { required: false, message: '请输入联系电话', trigger: 'blur' },
     { pattern: /^1[3-9]\d{9}$/, message: '请输入正确的手机号码', trigger: 'blur' }
   ],
   contactEmail: [
-    { required: true, message: '请输入联系邮箱', trigger: 'blur' },
+    { required: false, message: '请输入联系邮箱', trigger: 'blur' },
     { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur' }
   ]
 }
