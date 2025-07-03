329f3e96d3b0f9701fc3b48ef46fa79c85a4639c
keepersay <8269838+keepersay@user.noreply.gitee.com>
2025-07-04 00:55:06 +0800

    添加资源管理容器组件

---
+++ src/views/ResourceManagement/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="resource-management">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+// 资源管理容器组件
+</script>
+
+<style scoped>
+.resource-management {
+  height: 100%;
+  width: 100%;
+}
+</style> 
